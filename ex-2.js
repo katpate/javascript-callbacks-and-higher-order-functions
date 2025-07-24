698996a3874608d3820d60864c9cb289113a9f20
fn <j.k.13@hotmail.com>
2025-07-24 14:10:12 +0700

    feat: assignment javascript callback and higher order function

--- ex-2.js
+++ ex-2.js
@@ -3,29 +3,41 @@ const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
-function checkGradeOver70(score) {
-  // Start coding here
-}
+const checkGradeOver70 = (score) => {
+  return score > 70 ? true : false;
+};
 
-function atLeastFive(array, operation) {
-  // Start coding here
-}
+const atLeastFive = (array, operation) => {
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (operation(array[i])) {
+      count++;
+    }
+  }
+  return count >= 5;
+};
 
 // Using `atLeastFive` function here
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, checkGradeOver70);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, checkGradeOver70);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, checkGradeOver70);
 
 console.log(scoreRoom1Result); //true
 console.log(scoreRoom2Result); //false
 console.log(scoreRoom3Result); //false
 
+
 /*
 ====================================
 
 1. ใน Exercise นี้ ฟังก์ชันใดเป็น Callback Function?
-
+atLeastFive(studentScoresRoom1, checkGradeOver70);
+atLeastFive(studentScoresRoom2, checkGradeOver70);
+atLeastFive(studentScoresRoom3, checkGradeOver70);
+เพราะว่า checkGradeOver70 ถูกส่งเข้าไปเป็นพารามิเตอร์ของฟังก์ชัน atLeastFive
 2. ใน Exercise นี้ ฟังก์ชันใดเป็น Higher Order Function?
+function atLeastFive(array, operation)
+เพราะว่า atLeastFive รับฟังก์ชันอื่นมาเป็นพารามิเตอร์ และเรียกใช้งานฟังก์ชันนั้นภายในตัวเอง
 
 ====================================
 */
